f6883fc9ca1f40d26860d28d2d2a3cc8ae9b4e50
Pathum Sandaruwan <iit18055@std.uwu.ac.lk>
2022-10-02 22:50:02 +0530

    methods file

---
+++ src/method.java
@@ -0,0 +1,20 @@
+public class method {
+    public static void main(String[] args) {
+        //method is a block of code that execute whenever it is called
+        //method has to b create outside the main method
+        //if you are passing arguments in the method they don't need to be the same
+
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(addition(num1,num2));
+
+
+    }
+//    static void helloMethod(String name,int numbers){
+//        System.out.println("Hello "+name+" "+ numbers);
+//    }
+    static int addition(int number1, int number2){
+       int add;
+       return add=number1+number2;
+    }
+}
