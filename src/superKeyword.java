76a1a9cd72c62c9782e0e6d798928de843034267
Pathum Sandaruwan <iit18055@std.uwu.ac.lk>
2022-10-08 08:49:43 +0530

    superKeyword has been covered

---
+++ src/superKeyword.java
@@ -0,0 +1,24 @@
+public class superKeyword {
+    public static class animals{
+        String name;
+        int age;
+        animals(String name,int age){
+            this.name=name;
+            this.age=age;
+        }
+    }
+    public static class dog extends animals{
+        int numbersOfLegs;
+
+        dog( String name,int age, int numbersOfLegs){
+            super(name, age);
+            this.numbersOfLegs=numbersOfLegs;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        dog germanSherped = new dog("Boby",4,4);
+        System.out.println(germanSherped.name);
+    }
+}
